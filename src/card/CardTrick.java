c964b146a1d84868d09acfcd70e1465915fcbc8c
Aamir1923 <pateaami@sheridancollege.ca>
2024-02-01 00:32:40 -0500

    First

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -10,23 +10,51 @@ package card;
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author srinivsi
  */
+
+import java.util.Scanner;
+
 public class CardTrick {
     
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Card[] magicHand = new Card[7];
         
-        for (int i=0; i<magicHand.length; i++)
-        {
+        for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            int cardNumberRand = (int) (Math.random() * 13) + 1;
+            c.setValue(cardNumberRand);  // Random value between 1 and 13
+            
+            int cardSuitRand = (int) (Math.random() * 4);
+            c.setSuit(Card.SUITS[cardSuitRand]);  // Random suit
+            magicHand[i] = c;
+            
+            System.out.println("Card: " + cardNumberRand + " of " + Card.SUITS[cardSuitRand] + "(" + cardSuitRand + ")");
         }
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-        // add one luckcard hard code 2,clubs
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your lucky card value (1-13): ");
+        int luckyValue = scanner.nextInt();
+        System.out.print("Enter your lucky card suit (0=Hearts, 1=Diamonds, 2=Spades, 3=Clubs): ");
+        int luckySuit = scanner.nextInt();
+        
+        Card luckyCard = new Card();
+        luckyCard.setValue(luckyValue);
+        luckyCard.setSuit(Card.SUITS[luckySuit]);
+        
+        // Search for the lucky card in magicHand
+        boolean Report = false;
+        for (Card card : magicHand) {
+            if (card.getValue() == luckyCard.getValue() && card.getSuit().equals(luckyCard.getSuit())) {
+                Report = true;
+                break;
+            }
+            
+        }
+        
+        // Report the result
+        if (Report) {
+            System.out.println("Congratulations! Your lucky card is in the magic hand.");
+        } else {
+            System.out.println("Sorry, your lucky card is not in the magic hand.");
+        }
     }
-    
 }
